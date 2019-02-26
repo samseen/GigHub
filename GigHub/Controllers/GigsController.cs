631f8ac0af13d5f8124a909e39d04c5a6c58e7a1
Samson Akanbi <samson.akanbi@outlook.com>
2019-02-26 15:25:04 +0100

    Remodified the Create form to use @using to post the form.

--- GigHub/Controllers/GigsController.cs
+++ GigHub/Controllers/GigsController.cs
@@ -16,6 +16,7 @@ namespace GigHub.Controllers
             _context = new ApplicationDbContext();
         }
         
+        [Authorize]
         public ActionResult Create()
         {
             var viewModel = new GigFormViewModel
