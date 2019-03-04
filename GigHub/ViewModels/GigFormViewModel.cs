d9f872cdc6ff6efc0f1679c40223eb719bca8939
Samson Akanbi <samson.akanbi@outlook.com>
2019-03-04 17:06:15 +0100

    Invalid annotation now works on the Gig Model.

--- GigHub/ViewModels/GigFormViewModel.cs
+++ GigHub/ViewModels/GigFormViewModel.cs
@@ -12,6 +12,7 @@ namespace GigHub.ViewModels
         [Required]
         public string Venue { get; set; }
         [Required]
+        [FutureDate]
         public string Date { get; set; }
 
         [Required]
